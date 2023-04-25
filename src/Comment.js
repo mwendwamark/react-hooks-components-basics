96a09e4f239b9e7b84dc3e312133e1f164da46ff
mark mwendwa <mwendwamark310@gmail.com>
2023-04-25 09:06:09 +0300

    completed

--- src/Comment.js
+++ src/Comment.js
@@ -1,7 +1,13 @@
 import React from "react";
 
-function Comment() {
-  return <div>Naturally, I agree with this article.</div>;
+// function Comment() {
+//   return <div>Naturally, I agree with this article.</div>;
+// } 
+class Comment extends React.Component{
+  render() {
+  return (
+    <div> Hello! My name is Mark</div>
+  )}
 }
 
 export default Comment;
